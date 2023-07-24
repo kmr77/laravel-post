05c3c47f34490b4bf8874668a2e72fea57f03b06
KIMURA MASAYUKI <>
2023-07-24 11:05:45 +0900

    ♻️ topページレイアウト

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -14,15 +14,34 @@
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
-    <body class="font-sans text-gray-900 antialiased">
-        <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100">
-            <div>
-                <a href="/">
-                    <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
-                </a>
+    <body>
+        <div class="font-sans text-gray-900 antialiased">
+            {{-- ここから貼り付けます --}}
+            <div class="w-full container mx-auto">
+                <div class="w-full flex items-center justify-between">
+                    <a href="{{route('top')}}">
+                        <img src="{{asset('logo/inu-logo.jpg')}}" style="max-height:80px;" alt="">
+                    </a>
+                    <div class="flex w-1/2 justify-end content-center">
+                        {{-- ログイン・登録部分 --}}
+                        @if (Route::has('login'))
+                        <div class="hidden top-0 right-0 px-6 py-4 sm:block">
+                            @auth
+                                <a href="{{ url('/post') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">HOME</a>
+                            @else
+                                <a href="{{ route('login') }}" class="text-sm text-gray-700 dark:text-gray-500 underline font-bold text-base">ログイン</a>
+        
+                                @if (Route::has('register'))
+                                    <a href="{{ route('register') }}" class="ml-4 text-sm text-gray-700 dark:text-gray-500 underline font-bold text-base">登録</a>
+                                @endif
+                            @endauth
+                        </div>
+                        @endif
+                    </div>
+                </div>
             </div>
-
-            <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg">
+            {{-- ここまで貼り付けた箇所 --}}
+            <div class="w-full container mx-auto">
                 {{ $slot }}
             </div>
         </div>
