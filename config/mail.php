3c1b6574e9dbbe051544aaf434c13671748fd46e
KIMURA MASAYUKI <>
2023-07-25 09:24:34 +0900

    ✨ 問い合わせフォーム作成

--- config/mail.php
+++ config/mail.php
@@ -103,6 +103,9 @@ return [
         'name' => env('MAIL_FROM_NAME', 'Example'),
     ],
 
+    'admin' => env('MAIL_ADMIN', null),
+
+
     /*
     |--------------------------------------------------------------------------
     | Markdown Mail Settings
