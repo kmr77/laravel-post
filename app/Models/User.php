3ac7b94a5ce42cd1de58980aee00541e0c615841
KIMURA MASAYUKI <>
2023-07-25 11:02:02 +0900

    ✨ ロールの作成

--- app/Models/User.php
+++ app/Models/User.php
@@ -20,6 +20,10 @@ class User extends Authenticatable
         return $this->hasMany(Comment::class);
     }
 
+    public function roles(){
+        return $this->belongsToMany(Role::class);
+    }
+
     /**
      * The attributes that are mass assignable.
      *
