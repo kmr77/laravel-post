b1e6171e553c9b74ebe26a28e0f19ce443bf4c5d
KIMURA MASAYUKI <>
2023-07-25 15:56:34 +0900

    ✨ メール認証機能実装

--- routes/web.php
+++ routes/web.php
@@ -21,29 +21,36 @@ Route::get('/', function () {
     return view('welcome');
 })->name('top');
 
-Route::get('post/mypost', [PostController::class, 'mypost'])->name('post.mypost');
-Route::get('post/mycomment', [PostController::class, 'mycomment'])->name('post.mycomment');
-Route::resource('post', PostController::class);
-
-Route::post('post/comment/store',[CommentController::class, 'store'])->name('comment.store');
+// Route::get('/dashboard', function () {
+//     return view('dashboard');
+// })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::get('contact/create', [ContactController::class, 'create'])->name('contact.create');
 Route::post('contact/store', [ContactController::class, 'store'])->name('contact.store');
+
+Route::middleware(['verified'])->group(function(){
+    Route::get('post/mypost', [PostController::class, 'mypost'])->name('post.mypost');
+    Route::get('post/mycomment', [PostController::class, 'mycomment'])->name('post.mycomment');
+    Route::resource('post', PostController::class);
+    Route::post('post/comment/store',[CommentController::class, 'store'])->name('comment.store');
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    // 管理者用画面
+    Route::middleware(['auth', 'can:admin'])->group(function () {
+        Route::get('profile/index', [ProfileController::class, 'index'])->name('profile.index');
+    });
+});
+
+
+
 //↓同じコントローラを使用する時のまとめる書き方
 // Route::controller(ContactController::class)->group(function(){
 //     Route::get('contact/create', 'create')->name('contact.create');
 //     Route::post('contact/store', 'store')->name('contact.store');
 // });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::middleware(['auth', 'can:admin'])->group(function () {
-    Route::get('profile/index', [ProfileController::class, 'index'])->name('profile.index');
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
+
 
 require __DIR__.'/auth.php';
