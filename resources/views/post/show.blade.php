eac4151e7874f69a3fa78bd4e85e20e2fd69d9fa
KIMURA MASAYUKI <>
2023-07-26 11:02:41 +0900

    ✨ アバター登録機能実装

--- resources/views/post/show.blade.php
+++ resources/views/post/show.blade.php
@@ -11,12 +11,19 @@
     <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div class="mx-4 sm:p-8">
             <div class="px-10 mt-4">
-
-                <div class="bg-white w-full  rounded-2xl px-10 py-8 shadow-lg hover:shadow-2xl transition duration-500">
+                {{-- 修正部分 --}}
+                    <div class="bg-white w-full  rounded-2xl px-10 pt-2 pb-8 shadow-lg hover:shadow-2xl transition duration-500">
                         <div class="mt-4">
-                            <h1 class="text-lg text-gray-700 font-semibold">
-                                {{ $post->title }}
-                            </h1>
+                            <div class="flex">
+                                <div class="rounded-full w-12 h-12">
+                                    {{-- アバター表示 --}}
+                                    <img src="{{asset('storage/avatar/'.($post->user->avatar??'user_default.jpg'))}}">
+                                </div>
+                                <h1 class="text-lg text-gray-700 font-semibold hover:underline cursor-pointer float-left pt-4">
+                                    <a href="{{route('post.show', $post)}}">{{ $post->title }}</a>
+                                </h1>
+                            </div>
+                        {{-- 修正部分ここまで --}}
                             <hr class="w-full">
                         </div>
                         @can('update', $post)
@@ -45,7 +52,12 @@
                     <div class="bg-white w-full  rounded-2xl px-10 py-8 shadow-lg mt-8 whitespace-pre-line">
                         {{$comment->body}}
                         <div class="text-sm font-semibold flex flex-row-reverse">
-                            <p> {{ $comment->user->name }} • {{$comment->created_at->diffForHumans()}}</p>
+                            {{-- クラスを変更 --}}
+                            <p class="float-left pt-4"> {{ $comment->user->name }} • {{$comment->created_at->diffForHumans()}}</p>
+                            {{-- アバター追加 --}}
+                            <span class="rounded-full w-12 h-12">
+                            <img src="{{asset('storage/avatar/'.($comment->user->avatar??'user_default.jpg'))}}">
+                            </span>
                         </div>
                     </div>
                     @endforeach
